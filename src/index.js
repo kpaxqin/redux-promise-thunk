e7f5a89a130ffd85205a889b0e940c35db2a5920
Junwen Qin <jwqin@thoughtworks.com>
2016-05-26 23:58:59 +0800

    add support for customize actionType for each step with the first arg of createPromiseThunk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-const steps = {
-  START: 'START',
-  COMPLETED: 'COMPLETED',
-  FAILED: 'FAILED'
+const START = 'START';
+const COMPLETED = 'COMPLETED';
+const FAILED = 'FAILED';
+let steps = {
+  [START]: 'START',
+  [COMPLETED]: 'COMPLETED',
+  [FAILED]: 'FAILED'
 };
 
 function createPromiseThunk(type, promiseCreator, metaCreator) {
 
-  const getType = getTypeByStep(type);
+  const typeMap = getTypeMap(type);
+  const getType = step => typeMap[step];
   const getMeta = isFunction(metaCreator) ? metaCreator : getMetaByStep(metaCreator);
 
   function createActionForStep(step, payload) {
@@ -14,15 +18,15 @@ function createPromiseThunk(type, promiseCreator, metaCreator) {
   }
 
   return data => dispatch => {
-    dispatch(createActionForStep(steps.START, data));
+    dispatch(createActionForStep(START, data));
 
     const promise = promiseCreator.call(this, data);
 
     if (promise && promise.then) {
       promise.then((result)=>{
-        dispatch(createActionForStep(steps.COMPLETED, result));
+        dispatch(createActionForStep(COMPLETED, result));
       }, (err)=>{
-        dispatch(createActionForStep(steps.FAILED, err));
+        dispatch(createActionForStep(FAILED, err));
       });
     } else {
       throw new TypeError('The return of promiseCreator must be a promise object');
@@ -30,11 +34,30 @@ function createPromiseThunk(type, promiseCreator, metaCreator) {
   };
 }
 
-const setupSteps = customSteps => ({steps, ...customSteps});
+const setupSteps = customSteps => steps = {steps, ...customSteps};
 
+const isArray = Array.isArray;//todo: polyfill
 const isFunction = target => typeof target === 'function';
 
-const getTypeByStep = type => step => `${type}_${step}`;
+const getDefaultTypeByStep = type => step => `${type}_${steps[step]}`;
+
+const getTypeMap = type => {
+  if (isArray(type)) {
+    const [startType, completedType, failedType] = type;
+
+    return {
+      [START]: startType,
+      [COMPLETED]: completedType,
+      [FAILED]: failedType
+    }
+  } else {
+    return {
+      [START]: getDefaultTypeByStep(type)(START),
+      [COMPLETED]: getDefaultTypeByStep(type)(COMPLETED),
+      [FAILED]: getDefaultTypeByStep(type)(FAILED)
+    };
+  }
+};
 
 const getMetaByStep = meta => step => ({asyncStep: step, ...meta});
 
